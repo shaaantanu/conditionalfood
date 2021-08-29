473d3b8214112a783073f009363add4f27383185
shaaantanu <87699595+shaaantanu@users.noreply.github.com>
2021-08-29 20:40:35 +0530

    created by shantanu
    C program for conditional food items

---
+++ conditionalfood.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main() {
+int a;
+scanf("%d",&a);
+switch (a){
+case (1):
+printf("Food Item - Pizza\nPrice - Rs 239");
+break;
+case (2):
+printf("Food Item - Burger\nPrice - Rs 129");
+break;
+case (3):
+printf("Food Item - Pasta\nPrice - Rs 179");
+break;
+case (4):
+printf("Food Item - French Fries\nPrice - Rs 99");
+break;
+case (5):
+printf("Food Item - Sandwich\nPrice - Rs 149");
+break;
+}
+return 0;
+}
